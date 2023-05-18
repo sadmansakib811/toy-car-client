04d0e06786da184709173ee77311138803d77546
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-18 23:44:44 +0200

    nav bar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,12 @@
+
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- src/components/Home/slider/Slider.jsx
+++ src/components/Home/slider/Slider.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useRef } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+
 import img1 from '../../../assets/ironman.jpg';
 import img2 from '../../../assets/batman.jpg';
 import img3 from '../../../assets/spider.jpg';
@@ -9,43 +9,34 @@ const Slider = () => {
   
 
   return (
-    <Carousel >
-    <Carousel.Item>
-      <img
-        className="d-block w-100 h-96"
-        src={img1}
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100 h-96"
-        src={img2}
-        alt="Second slide"
-      />
-      <Carousel.Caption>
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100 h-96"
-        src={img3}
-        alt="Third slide"
-      />
-      <Carousel.Caption>
-        <h5>Third slide label</h5>
-        <p>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-        </p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+    <div className="carousel w-full">
+    <div id="slide1" className="carousel-item relative w-full">
+      <img src={img1} className="w-full" style={{height:'700px'}} />
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide4" className="btn btn-circle">❮</a> 
+        <button className="btn btn-secondary ">Shop Now</button> {/* Added button */}
+        <a href="#slide2" className="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide2" className="carousel-item relative w-full">
+      <img src={img2} className="w-full" style={{height:'700px'}}/>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide1" className="btn btn-circle">❮</a> 
+        <button className="btn btn-secondary ">Shop Now</button> {/* Added button */}
+       
+        <a href="#slide3" className="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide3" className="carousel-item relative w-full">
+      <img src={img3} className="w-full" style={{height:'700px'}}/>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide2" className="btn btn-circle">❮</a> 
+        <button className="btn btn-secondary ">Shop Now</button> {/* Added button */}
+        <a href="#slide4" className="btn btn-circle">❯</a>
+      </div>
+    </div> 
+   
+  </div>
     );
 };
 
